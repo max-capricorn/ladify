47fe772bb36e9cb7f08305511bf991ca380475be
zk <liuzq7@gmail.com>
2021-12-01 14:52:23 +0800

    chore: init

---
+++ packages/web/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Hello } from '@ladify/ui'
+import { add } from '@ladify/utils'
+import ReactDOM from 'react-dom'
+
+export const App = () => {
+  return (
+    <div>
+      <Hello />
+      <br />
+      {add(1, 2)}
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ readme.md
@@ -0,0 +1,1 @@
+# PNPM Workspace Monorepo Template
